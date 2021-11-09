133ad73a8ee133bd73ca4184df143b34437e2175
Roshan Saravanan <roshan.saravanan001@gmail.com>
2021-11-09 20:31:15 +0530

    half completed dataset.py returns a sequence of melspectrograms. 66xDIM [DIM depends on length of audio]

---
+++ neural_net/src/dataset.py
@@ -0,0 +1,45 @@
+import torch
+import torchaudio
+import pandas as pd
+from torch.utils.data import Dataset
+from glob import glob
+
+
+class FetchAudio(Dataset):
+	"""docstring for FetchAudio"""
+	def __init__(self, 
+			path_to_csv = '../../data/merged_df.csv', 
+			path_to_dir = "../../data/train",
+			SAMPLE_RATE = 22050,
+		):
+
+		super(FetchAudio, self).__init__()
+		self.audio_paths = glob(path_to_dir + "/*.mp3")
+		self.df = pd.read_csv(path_to_csv, index_col="Video_id")
+		self.SAMPLE_RATE = SAMPLE_RATE
+		self.transformation = torchaudio.transforms.MelSpectrogram(
+			sample_rate=SAMPLE_RATE,
+			n_fft=1024,
+			hop_length=512,
+			n_mels=64)
+
+
+	def __len__(self):
+		return len(self.audio_paths)
+
+
+	def __getitem__(self, idx):
+		audio_path = self.audio_paths[idx]
+		audio_id = audio_path.split(" - ")[-1].split('.')[-2]
+		signal, sr = torchaudio.load(audio_path)
+
+		if sr != self.SAMPLE_RATE:
+			resampler = torchaudio.transforms.Resample(sr, self.SAMPLE_RATE)
+			signal = resampler(signal)
+
+		if signal.shape[0] > 1:
+			signal = torch.mean(signal, dim=0, keepdim=True)
+
+		mel_spectrogram = self.transformation(signal)
+
+		return mel_spectrogram[0]
