30a4b62c5518137621e9074cff484a3f7bc86751
jdranpariya <jdpatel.code@gmail.com>
2022-06-14 18:15:55 +0530

    added transcript_download.py

--- data/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-print(np.mean([2, 5, 6, 8, 7]))
-print(np.mode([2, 5, 6, 8, 7]))
-print(np.median([2, 5, 6, 8, 7]))
---
+++ data/transcript_download.py
@@ -0,0 +1,18 @@
+import json
+import pandas as pd
+from tqdm import tqdm
+import time
+from youtube_transcript_api import YouTubeTranscriptApi
+
+df = pd.read_csv('sponsorTimes.csv')
+
+videoIds = df['videoID'].unique()
+
+for video_id in tqdm(videoIds):
+    try:
+        time.sleep(0.25)
+        json_transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        with open(f'transcripts/{video_id}.json', 'w') as f:
+            json.dump(json_transcript,f)
+    except:
+        pass
